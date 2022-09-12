0c97687ef774108942e47d1e4b8119b7657ce080
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2022-09-12 15:50:22 +0300

    02 video up date

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import '../css/common.css';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_KEY = 'videoplayer-current-time';
 
+const onPlay = function (data){
+    const stringifyData = JSON.stringify(data);
+    localStorage.setItem(TIME_KEY,stringifyData);
+
+};
+player.on('timeupdate', throttle(onPlay,1000));
