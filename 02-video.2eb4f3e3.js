612b818da01f81d5181f4f18ad60fbe3bcfce30c
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2022-09-13 09:59:59 +0000

    Deploying to gh-pages from @ leshazimoglyad/goit-js-hw-08@5f1326247c49e7cbc55cbe21e1d851958337f944 🚀

--- 02-video.2eb4f3e3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("timeupdate",throttle((function(e){var t=e.seconds;localStorage.setItem("videoplayer-current-time",t)}),1e3)),t.setCurrentTime(localStorage.getItem("videoplayer-current-time"))}();
-//# sourceMappingURL=02-video.2eb4f3e3.js.map
