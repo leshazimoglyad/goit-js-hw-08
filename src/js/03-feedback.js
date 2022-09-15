c745b15133b0a596296cc711e2b3cff3b539310a
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2022-09-15 15:49:04 +0300

    іаа8вя

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,9 +4,9 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
-const formData = {};
 
 function onFormData(e) {
+  let formData = localStorage.getItem('feedback-form-state');    formData = formData ? JSON.parse(formData) : {};
   formData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
@@ -19,10 +19,11 @@ function onSubmitForm(e) {
 }
 
 (function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  let data =  localStorage.getItem('feedback-form-state');
   const email = document.querySelector('.feedback-form input');
   const message = document.querySelector('.feedback-form textarea');
   if (data) {
+    data  = JSON.parse(data );
     email.value = data.email;
     message.value = data.message;
   }
